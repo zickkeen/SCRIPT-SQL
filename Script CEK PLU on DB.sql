5682e45f7ef64dfc1c470c0d79917fe4d393970f
Zick Keen <jackkinny@gmail.com>
2017-01-16 19:17:04 +0800

    Update

--- Script CEK PLU on DB.sql
+++ Script CEK PLU on DB.sql
@@ -1,11 +1,19 @@
 SELECT a.PLUNumber
 	, a.PLUName
 	, b.[DepartmentName]
-	, a.[Sell1]
-	, a.[sell2]
-	, a.[KP1]
+	, replace(convert(varchar,convert(Money, a.[Sell1]),1),'.00','') as [Sell1]
+	, replace(convert(varchar,convert(Money, a.[sell2]),1),'.00','') as [Sell2]
+	, c.[KPID] as KP1
+	, d.[KPID] as KP2
+	--, e.[KPID] as KP3
 	, a.[MasterKPPrint]
 FROM PLU a
 	, DEPARTMENTS b
+	, KPDetails c
+	, KPDetails d
+	, KPDetails e
 where b.[DepartmentNo]=a.[Department]
-	AND [SELL1] !=0
+	AND c.PrinterID = a.KP1
+	AND d.PrinterID = a.KP2
+	--AND e.PrinterID = a.KP3
+	--AND [SELL1] !=0
